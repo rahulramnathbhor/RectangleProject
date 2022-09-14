64873811471a62f9b48f2facb91199278a607d7a
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 20:53:56 +0530

    Add project files.

---
+++ RectangleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(1, 2);
+            Console.WriteLine(r.display());
+        }
+    }
+}
---
+++ RectangleProject/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleProject
+{
+    internal class Rectangle
+    {
+        int l, b, p;
+       
+            public Rectangle(int l,int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public int calculate()
+        {
+            { return p = 2 * (l + b); }
+        }
+        public int display()
+            { return p; }
+            
+    }
+}
